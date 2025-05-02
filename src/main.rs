290bdcffdc87c3feb05e4b3f9df46b01777bd436
Konstantinos Stathopulos <19920164+stathopulos@users.noreply.github.com>
2025-05-01 22:29:57 -0600

    command line program to take a string as an argument

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
-use display_tree::println_tree;
 use huffco::HuffmanTree;
+use std::env;
 
-fn main() {
-    let s = "Huffman for fun";
-    let t = HuffmanTree::tree(s).unwrap();
-    let e = t.enc(s);
+use display_tree::println_tree;
 
-    println_tree!(t);
-    println!("string: \"{s}\"");
-    println!("encoded: {e:b}",);
-    println!("decoded: \"{}\"", t.dec(&e));
+fn parse_args(args: &mut env::Args) -> Result<String, &'static str> {
+    match (args.next(), args.next(), args.next()) {
+        (_, _, Some(_)) => {
+            Err("Too many arguments! This program only accepts a single string as input!")
+        }
+        (_, None, _) => {
+            Err("No arguments provided! This program requires a single string as input!")
+        }
+        (_, Some(s), None) => Ok(s),
+    }
+}
 
-    println!("Number of bits for string: {}", 8 * s.len());
-    println!("Number of bits compressed: {}", e.len());
+fn main() -> Result<(), &'static str> {
+    let input_string = parse_args(&mut env::args())?;
+    let tree = HuffmanTree::tree(&input_string)
+        .ok_or("Input string is empty! Cannot create a huffman tree from an empty string!")?;
+    let encoded_bits = tree.enc(&input_string);
+
+    println_tree!(tree);
+    println!("string: \"{input_string}\"");
+    println!("encoded: {encoded_bits:b}",);
+    println!("└── back decoded: \"{}\"", tree.dec(&encoded_bits));
+
+    println!("Number of bits in 8-bit ASCII: {}", 8 * input_string.len());
+    println!("Number of bits compressed: {}", encoded_bits.len());
+    Ok(())
 }
